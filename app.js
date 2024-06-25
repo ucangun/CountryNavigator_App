9e6735d636563de9beca3851c298a7f135671d2b
ucangun <149247682+ucangun@users.noreply.github.com>
2024-06-25 22:05:11 +0200

    get data from geocodingAPI with data of current Location

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
+const otherButton = document.querySelector(".other");
+const whereButton = document.querySelector(".where");
+
 // ! Geolocation Function
+let lat;
+let long;
 
 navigator.geolocation.getCurrentPosition((pos) => {
   const { latitude: lat, longitude: long } = pos.coords;
-  console.log(lat, long);
+
+  getLocation(lat, long);
+});
+
+// ! Geocoding API
+
+const getLocation = async (lat, long) => {
+  const res = await fetch(`https://geocode.xyz/${lat},${long}?geoit=json`);
+
+  const data = await res.json();
+  //console.log(data);
+  console.log(`You are currently in ${data.city} , ${data.country} `);
+};
+
+whereButton.addEventListener("click", () => {
+  getLocation(lat, long);
 });
