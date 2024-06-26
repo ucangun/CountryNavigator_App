f5e7a3456ccb1820c36095708c413769e1dbb45e
ucangun <149247682+ucangun@users.noreply.github.com>
2024-06-26 17:39:29 +0200

    code refactoring part1

--- app.js
+++ app.js
@@ -8,13 +8,11 @@ const displayContainer = document.querySelector(".displayContainer");
 const inputContainer = document.querySelector(".inputContainer");
 
 // ! Geolocation Function
-let lat;
-let long;
+let lat, long;
 
 navigator.geolocation.getCurrentPosition((pos) => {
-  const { latitude: lat, longitude: long } = pos.coords;
-
-  getLocation(lat, long);
+  lat = pos.coords.latitude;
+  long = pos.coords.longitude;
 });
 
 // ! Geocoding API
@@ -23,76 +21,75 @@ let countryName;
 let country;
 
 const getLocation = async (lat, long) => {
-  const res = await fetch(`https://geocode.xyz/${lat},${long}?geoit=json`);
-
-  const data = await res.json();
-  //console.log(`You are currently in ${data.city} , ${data.country} `);
-  country = data;
-  countryName = data.country;
+  try {
+    const res = await fetch(`https://geocode.xyz/${lat},${long}?geoit=json`);
+    if (!res.ok) {
+      throw new Error(`The country could not be found 😔 ${res.status}`);
+    }
+    const data = await res.json();
+    country = data;
+    countryName = data.country;
+    printAddress(country);
+  } catch (error) {
+    displayFlag.textContent = `${error}`;
+  }
 };
 
 const printAddress = (country) => {
   const html = `
   <p class="adressText">You are currently in ${country.city} , ${country.country}</p>`;
 
-  displayAdress.insertAdjacentHTML("beforeend", html);
+  displayAdress.innerHTML = html;
 };
 
 // ! Country API
 
 const getCountry = async (countryName) => {
-  const res = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
-  const data = await res.json();
-  seeLocation(data);
-  //console.log(data);
-  data.forEach((data) => printLocationCard(data));
+  try {
+    const res = await fetch(
+      `https://restcountries.com/v3.1/name/${countryName}`
+    );
+    if (!res.ok) {
+      throw new Error(`Country data could not be retrieved 😔 ${res.status}`);
+    }
+    const data = await res.json();
+    seeLocation(data);
+    data.forEach((data) => printLocationCard(data));
+  } catch (error) {
+    console.error(`Error fetching country data: ${error}`);
+    displayFlag.textContent = `${error}`;
+  }
 };
 
 const printLocationCard = (data) => {
   const html = `
-<div class="card  p-0" style="width: 25rem;">
-  <img src="${data.flags.svg}" class="card-img-top card-img"   />
-  <div class="card-body d-flex flex-column justify-content-between">
-    <h5 class="card-title">${data.name.common}</h5>
-    <ul class="card-list list-unstyled me-2 fs-2">
-      <li><i class="fa-solid fa-landmark"></i> ${data.capital}</li>
-      <li><i class="fa-solid fa-coins"></i> ${
-        Object.values(data.currencies)[0].name
-      }</li>
-      <li><i class="fa-solid fa-person"></i> ${(
-        Number(data.population) / 1000000
-      ).toFixed(2)} </li>
-    </ul>
-    <a href="#" class="btn btn-primary btn-lg location">Location!</a>
-  </div>
-  </div> 
-  `;
-
-  displayFlag.insertAdjacentHTML("beforeend", html);
+  <div class="card p-0" style="width: 25rem;">
+    <img src="${data.flags.svg}" class="card-img-top card-img" />
+    <div class="card-body d-flex flex-column justify-content-between">
+      <h5 class="card-title">${data.name.common}</h5>
+      <ul class="card-list list-unstyled me-2 fs-2">
+        <li><i class="fa-solid fa-landmark"></i> ${data.capital}</li>
+        <li><i class="fa-solid fa-coins"></i> ${
+          Object.values(data.currencies)[0].name
+        }</li>
+        <li><i class="fa-solid fa-person"></i> ${(
+          Number(data.population) / 1000000
+        ).toFixed(2)}</li>
+      </ul>
+      <a href="#" class="btn btn-primary btn-lg location">Location!</a>
+    </div>
+  </div>`;
+  displayFlag.innerHTML = html;
 };
-
-//! Event Listeners
-
-whereButton.addEventListener("click", () => {
-  getLocation(lat, long);
-  printAddress(country);
-  getCountry(countryName);
-});
-
-otherButton.addEventListener("click", () => {
-  getOtherCountry();
-});
 
 //! Other Buttons
 
 const getOtherCountry = async () => {
   const res = await fetch("https://restcountries.com/v3.1/all");
   const data = await res.json();
-  console.log(data);
 
-  input(data);
   seeLocation(data);
-
+  input(data);
   displayRow.innerHTML = "";
   displayContainer.classList.add("d-none");
   inputContainer.classList.remove("d-none");
@@ -125,6 +122,17 @@ const printCountryCard = (data) => {
   displayRow.insertAdjacentHTML("beforeend", html);
 };
 
+const input = (data) => {
+  document.querySelector("input").oninput = (e) => {
+    const filteredData = data.filter((country) =>
+      country.name.common.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+
+    displayRow.innerHTML = "";
+    filteredData.forEach((country) => printCountryCard(country));
+  };
+};
+
 const seeLocation = (data) => {
   document.addEventListener("click", (e) => {
     if (e.target.classList.contains("location")) {
@@ -143,13 +151,13 @@ const seeLocation = (data) => {
   });
 };
 
-const input = (data) => {
-  document.querySelector("input").oninput = (e) => {
-    const filteredData = data.filter((country) =>
-      country.name.common.toLowerCase().includes(e.target.value.toLowerCase())
-    );
+//! Event Listeners
 
-    displayRow.innerHTML = "";
-    filteredData.forEach((country) => printCountryCard(country));
-  };
-};
+whereButton.addEventListener("click", () => {
+  getLocation(lat, long);
+  getCountry(countryName);
+});
+
+otherButton.addEventListener("click", () => {
+  getOtherCountry();
+});
